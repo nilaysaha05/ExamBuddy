f6106771ea899c2fd9ba804e6bfb0a900bbcf1e9
nilaysaha05 <sahanilay.05@gmail.com>
2023-02-21 19:26:40 +0530

    firebase_database integration Done

--- lib/pages/file_upload_page.dart
+++ lib/pages/file_upload_page.dart
@@ -1,28 +1,68 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:mme_notes_app/colours.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+
 
 
 class UploadPage extends StatefulWidget {
- const  UploadPage({Key? key}) : super(key: key);
+  const UploadPage({Key? key}) : super(key: key);
 
   @override
   State<UploadPage> createState() => _UploadPageState();
 }
 
 class _UploadPageState extends State<UploadPage> {
-  bool loading = false;
-
+   File? file;
   final noteEditingController = TextEditingController();
   final dateEditingController = TextEditingController();
-  final String id= DateTime.now().microsecondsSinceEpoch.toString();
+  final String id = DateTime.now().microsecondsSinceEpoch.toString();
+  String pdfUrl='';
+  String _fileText = 'No file Selected';
 
-  final CollectionReference _reference = FirebaseFirestore.instance.collection('Steel_Pyq');
+  final CollectionReference _reference =
+      FirebaseFirestore.instance.collection('Steel_Pyq');
 
-@override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
+  void _pickFile () async
+  {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      //allowedExtensions: ['pdf'],
+    );
+    if(result!=null && result.files.single.path!=null)
+      {
+        PlatformFile files = result.files.first;
+       final path = result.files.single.path!;
+
+        setState(() {
+          file = File(path);
+          _fileText = files.name;
+        });
+      }
+    else{
+      return;
+    }
+
   }
+
+  Future _uploadFile () async
+  {
+    Reference referenceRoot = FirebaseStorage.instance.ref();
+    Reference referenceDirFile = referenceRoot.child('pyq').child(id+'Steel_pyq');
+    try{
+      await referenceDirFile.putFile(file!);
+      pdfUrl= await referenceDirFile.getDownloadURL();
+    }
+    catch(e)
+    {
+      ScaffoldMessenger.of(context).showSnackBar( SnackBar(content :Text(e.toString())));
+    }
+
+
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,34 +76,33 @@ class _UploadPageState extends State<UploadPage> {
         ),
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Column(
+          child: ListView(
             children: [
-          Row(
-          children: [
-          Padding(
-          padding:
-              const EdgeInsets.only(top: 8.0, bottom: 5.0),
-          child: Container(
-            height: 50,
-            width: 45,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: offWhite,
-            ),
-            child: IconButton(
-              onPressed: (){
-                Navigator.of(context).pop();
-              },
-              icon: const Icon(
-                Icons.arrow_back_ios_new_rounded,
-                size: 25,
-                color: blue,
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 8.0, bottom: 5.0),
+                    child: Container(
+                      height: 50,
+                      width: 45,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: offWhite,
+                      ),
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        icon: const Icon(
+                          Icons.arrow_back_ios_new_rounded,
+                          size: 25,
+                          color: blue,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ),
-        ),
-      ],
-    ),
               const SizedBox(
                 height: 30,
               ),
@@ -86,29 +125,48 @@ class _UploadPageState extends State<UploadPage> {
                 height: 20.0,
               ),
               Container(
-                padding: const EdgeInsets.all(8),
-                 decoration: BoxDecoration(border: Border.all(color: Colors.black26)),
+                  padding: const EdgeInsets.all(8),
+                  decoration:
+                      BoxDecoration(border: Border.all(color: Colors.black26)),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [const Text("File name"),IconButton(onPressed: (){}, icon: const Icon(Icons.upload))],
+                    children: [
+                       Text(_fileText),
+                      IconButton(
+                          onPressed:() {
+                            _pickFile();
+                          },
+
+                          icon: const Icon(Icons.attach_file_rounded))
+                    ],
                   )),
               const SizedBox(
                 height: 20.0,
               ),
-              ElevatedButton (onPressed: () {
+              ElevatedButton(
+                  onPressed: () async {
+                    await _uploadFile();
 
-                Map<String, String> dataToSend=
-                    {
+                    if(pdfUrl.isEmpty)
+                      {
+                        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content :Text('Please Upload a file..')));
+                        return;
+                      }
+
+
+                    Map<String, String> dataToSend = {
                       'note': noteEditingController.text.toString(),
-                      'date' : dateEditingController.text.toString(),
+                      'date': dateEditingController.text.toString(),
+                      'pdf' : pdfUrl,
                     };
 
-                _reference.doc(id).set(dataToSend);
+                    if(noteEditingController.text!=""&&dateEditingController.text!="") {
+                      _reference.doc(id).set(dataToSend);
+                      Navigator.of(context).pop();
+                    }
 
-                Navigator.of(context).pop();
-
-
-              }, child: const Text("Save")),
+                  },
+                  child: const Text("Save")),
             ],
           ),
         ));
